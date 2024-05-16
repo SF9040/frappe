2788a6974303905dd224038db2ac088a456f9214
Saber <saber.chfd@gmail.com>
2024-05-16 00:21:36 +0000

    add fixes for the multiselect dialog.

--- frappe/public/js/frappe/form/multi_select_dialog.js
+++ frappe/public/js/frappe/form/multi_select_dialog.js
@@ -81,8 +81,7 @@ frappe.ui.form.MultiSelectDialog = class MultiSelectDialog {
 		this.dialog = new frappe.ui.Dialog({
 			title: title,
 			fields: this.fields,
-			size: this.size,
-			cssClass: "modal-lg", 
+			size: "large",
 			primary_action_label: this.primary_action_label || __("Get Items"),
 			secondary_action_label: __("Make {0}", [__(this.doctype)]),
 			primary_action: () => {
